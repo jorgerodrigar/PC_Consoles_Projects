83f89d83fd524119f70b9988eb5095f27920a7e8
Jorge Rodríguez García <jorgerodrigar2703@gmail.com>
2019-12-14 00:07:36 +0100

    empezada la adaptacion de scrollManager, aun no incluido en gameManager

--- Practica2PC/src/Practica4/Logic/ScrollManager.cpp
+++ Practica2PC/src/Practica4/Logic/ScrollManager.cpp
@@ -1,5 +1,26 @@
 #include "ScrollManager.h"
 #include <Input/Input.h>
+#include <Renderer/Renderer.h>
+
+void ScrollManager::setNextTargetPositions()
+{
+	for (int i = 0; i < 4; i++) {
+		if (isOutsideBounds(_actualPositions[i])) {
+			if (_dir > 0)
+				_actualPositions[i] = ((32.0f*2.0f) - (192.0f*2.0f)); // PROVISIONAL
+			else if (_dir < 0)
+				_actualPositions[i] = (Renderer::getWindowWidth() - (32.0f*2.0f)); // PROVISIONAL
+		}
+		_targetPositions[i] = _actualPositions[i] + (_dir * 192 * 2);// PROVISIONAL
+	}
+
+	//send msg a puertas -> invisibles
+}
+
+bool ScrollManager::isOutsideBounds(float x) // PROVISIONAL
+{
+	return (x >= Renderer::getWindowWidth() - 32.0f*2.0f || x + (192 * 2) <= 32.0f*2.0f);
+}
 
 ScrollManager::ScrollManager()
 {
@@ -13,23 +34,66 @@ ScrollManager::~ScrollManager()
 {
 }
 
-void ScrollManager::update()
+void ScrollManager::init()
 {
-	_inputData = Input::getUserInput();
+	_sprite.init(Resources::marcoPuerta, 1, 1);
+	_dir = 0;
+	_x = 32 * 2; // PROVISIONAL
+	_y = 48 * 2;
 
-	if (!_scrollingLeft && _inputData.buttonsInfo.R1) { // scroll de la camara hacia la derecha
-		_scrollingRight = true;                         // (las puertas se moveran hacia la izquierda)
-		sendMessage(ScrollMessage(LATERAL_SCROLL, _vel, -1));
+	for (int i = 0; i < 4; i++) {
+		_actualPositions[0] = _x + (i*_sprite.getWidth() * 2);
 	}
-	else if (!_scrollingRight && _inputData.buttonsInfo.L1) { // scroll de la camara hacia la izquierda
-		_scrollingLeft = true;                                // (las puertas se moveran hacia la derecha)
-		sendMessage(ScrollMessage(LATERAL_SCROLL, _vel, 1));
-	}
+}
 
-	else if (_scrollingRight && !_inputData.buttonsInfo.R1) {
-		_scrollingRight = false;
+void ScrollManager::update(double deltaTime)
+{
+	if (_active && _dir != 0) {
+		for (int i = 0; i < 4; i++) {
+			_actualPositions[i] += _dir * _vel*deltaTime;
+			if (_dir > 0 && _actualPositions[i] >= _targetPositions[i] ||
+				_dir < 0 && _actualPositions[i] <= _targetPositions[i] || _dir == 0) {
+				_actualPositions[i] = _targetPositions[i];
+				_dir = 0;
+				// send msg dir + send msg puertas -> visible
+			}
+
+			setDirty();
+		}
 	}
-	else if (_scrollingLeft && !_inputData.buttonsInfo.L1) {
-		_scrollingLeft = false;
+}
+
+void ScrollManager::render(RendererThread * renderThread)
+{
+	if (_active && _pendingFrames >= 0) {
+		for (int i = 0; i < 4; i++) {
+			_sprite.render(_actualPositions[i], _y, renderThread);
+		}
+		_pendingFrames--;
+	}
+}
+
+void ScrollManager::handleInput()
+{
+	if (_active) {
+		_inputData = Input::getUserInput();
+
+		if (!_scrollingLeft && _inputData.buttonsInfo.R1) { // scroll de la camara hacia la derecha
+			_scrollingRight = true;                         // (las puertas se moveran hacia la izquierda)
+			_dir = -1;
+			setNextTargetPositions();
+		}
+		else if (!_scrollingRight && _inputData.buttonsInfo.L1) { // scroll de la camara hacia la izquierda
+			_scrollingLeft = true;                                // (las puertas se moveran hacia la derecha)
+			_dir = 1;
+			setNextTargetPositions();
+		}
+
+		else if (_scrollingRight && !_inputData.buttonsInfo.R1) {
+			_scrollingRight = false;
+		}
+		else if (_scrollingLeft && !_inputData.buttonsInfo.L1) {
+			_scrollingLeft = false;
+		}
 	}
 }
--- Practica2PC/src/Practica4/Logic/ScrollManager.h
+++ Practica2PC/src/Practica4/Logic/ScrollManager.h
@@ -1,19 +1,31 @@
 #pragma once
 #include <Utils/Emitter.h>
+#include <Logic/GameObject.h>
 #include <Input/InputData.h>
 
-class ScrollManager : public Emitter
+class ScrollManager : public GameObject, public Emitter
 {
 private:
 	InputData _inputData;
 	bool _scrollingRight, _scrollingLeft;
 	float _vel;
+	int _dir;
+
+	float _targetPositions[4];
+	float _actualPositions[4];
+
+	void setNextTargetPositions();
+	bool isOutsideBounds(float x);
 
 public:
 	ScrollManager();
 	ScrollManager(float vel);
 	virtual ~ScrollManager();
 
-	void update();
+	virtual void init();
+
+	virtual void update(double deltaTime);
+	virtual void render(RendererThread* renderThread);
+	virtual void handleInput();
 };
 
